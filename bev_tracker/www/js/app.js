2e2b5bccca6a3c05d53392b7bbc540641d201474
emilyls <snyderem@oregonstate.edu>
2016-07-26 22:44:00 -0700

    fixed modals in location detail v bev detail

--- bev_tracker/www/js/app.js
+++ bev_tracker/www/js/app.js
@@ -1,44 +1,17 @@
-// Ionic Starter App
+// Beverage Tracker App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ngCordova', 'ui.router', 'starter.controllers', 'starter.services'])
+angular.module('bev_tracker', ['ionic', 'ngCordova', 'ui.router', 'bev_tracker.controllers', 'bev_tracker.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-      if (cordova.platformId === 'ios' && window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
+  .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
-
-  // Each tab has its own nav history stack:
-
   .state('tab.new', {
     url: '/new',
     views: {
@@ -48,7 +21,6 @@ angular.module('starter', ['ionic', 'ngCordova', 'ui.router', 'starter.controlle
       }
     }
   })
-
   .state('tab.beverages', {
       url: '/beverages',
       views: {
@@ -81,12 +53,12 @@ angular.module('starter', ['ionic', 'ngCordova', 'ui.router', 'starter.controlle
     views: {
       'tab-location': {
         templateUrl: 'templates/location-detail.html',
-        controller: 'BeverageDetailCtrl'
+        controller: 'LocationDetailCtrl'
       }
     }
   });
 
-  // if none of the above states are matched, use this as the fallback
+  // efault tab
   $urlRouterProvider.otherwise('/tab/beverages');
 
 });
--- bev_tracker/www/js/controllers.js
+++ bev_tracker/www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('bev_tracker.controllers', [])
 
 .controller('NewCtrl', function ($scope, Beverages, $http) {
 
@@ -264,6 +264,201 @@ angular.module('starter.controllers', [])
     };
 
 })
+
+.controller('LocationDetailCtrl', function ($scope, $stateParams, Beverages, $ionicModal, $state) {
+
+    $scope.status;
+    $scope.beverage;
+    $scope.average;
+    $scope.bevStores;
+    $scope.otherStores;
+
+    $scope.price = {};
+    $scope.store = {};
+    $scope.review = {};
+
+
+    init();
+    function init() {
+        $scope.$on('$ionicView.enter', function (e) {
+            getBeverage();
+            getStores();
+        });
+    }
+
+    function getBeverage() {
+        total = 0;
+        count = 0;
+        average = 0;
+        Beverages.getBeverage($stateParams.beverageId)
+            .then(function (response) {
+                $scope.beverage = response.data[0];
+                count = $scope.beverage.ratings.length;
+                for (var i = 0; i < count; i++) {
+                    total += $scope.beverage.ratings[i].value;
+                }
+                $scope.average = total / count;
+            }, function (error) {
+                $scope.status = 'Unable to load beverage details ' + error.message;
+            });
+    }
+
+
+    function getStores() {
+        Beverages.getAllStores()
+            .then(function (response) {
+                var allStores = response.data;
+                $scope.bevStores = [];
+                $scope.otherStores = [];
+                for (var i = 0; i < allStores.length; i++) {
+                    var numBevs = allStores[i].prices.length;
+                    for (j = 0; j < numBevs; j++) {
+                        if (allStores[i].prices[j].beverage == $stateParams.beverageId) {
+                            var price = {
+                                store: allStores[i],
+                                price: allStores[i].prices[j]
+                            };
+                            $scope.bevStores.push(price);
+                            break;
+                        }
+                    }
+                    if (j == numBevs) {
+                        $scope.otherStores.push(allStores[i]);
+                    }
+                }
+            }, function (error) {
+                $scope.status = 'Unable to load stores: ' + error.message;
+            });
+    }
+
+    $ionicModal.fromTemplateUrl('addReviewLocation.html', {
+        id: '1',
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.oModal1 = modal;
+    });
+
+    $ionicModal.fromTemplateUrl('addPriceLocation.html', {
+        id: '2',
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.oModal2 = modal;
+    });
+
+    $ionicModal.fromTemplateUrl('addStoreLocation.html', {
+        id: '3',
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.oModal3 = modal;
+    });
+
+    $scope.openModal = function (index) {
+        if (index == 1) $scope.oModal1.show();
+        else if (index == 2) $scope.oModal2.show();
+        else {
+            $scope.store.country = 'USA';
+            $scope.oModal3.show();
+        }
+    };
+    $scope.closeModal = function (index) {
+        if (index == 1) {
+            $scope.review = {};
+            $scope.oModal1.hide();
+        }
+        else if (index == 2) {
+            $scope.price = {};
+            $scope.oModal2.hide();
+        }
+        else {
+            $scope.store = {};
+            $scope.oModal3.hide();
+        }
+    };
+    $scope.$on('$destroy', function () {
+        $scope.oModal1.remove();
+        $scope.oModal2.remove();
+        $scope.oModal3.remove();
+    });
+
+    $scope.AddReview = function (data) {
+        var rating = {
+            bev_id: parseInt($stateParams.beverageId, 10),
+            value: data.rating
+        };
+
+        if (data.notes) {
+            rating.notes = data.notes;
+        }
+
+        Beverages.addRating(encodeData(rating))
+            .then(function (response) {
+                console.log('Added review!');
+                getBeverage();
+            }, function (error) {
+                console.log(error);
+                $scope.status = 'Unable to add review: ' + error.message;
+            });
+        $scope.closeModal(1);
+    };
+
+    $scope.AddPrice = function (data) {
+        if (data.price && data.date && data.size && data.units && data.store_id) {
+            var newPrice = {
+                price: data.price,
+                day: data.date.getDate(),
+                month: data.date.getMonth() + 1,
+                year: data.date.getFullYear(),
+                size: data.size,
+                units: data.units,
+                store_id: data.store_id
+            };
+            newPrice.bev_id = parseInt($stateParams.beverageId, 10);
+
+            Beverages.addPrice(encodeData(newPrice))
+                .then(function (response) {
+                    console.log('Added Price!');
+                    getStores();
+                    getBeverage();
+                    $scope.price = {};
+                }, function (error) {
+                    console.log(error);
+                });
+        }
+        $scope.closeModal(2);
+    };
+
+    $scope.AddStore = function (data) {
+        if (data.name && data.street && data.city && data.state && data.country) {
+
+            Beverages.addStore(encodeData(data))
+                .then(function (response) {
+                    console.log('Added Store!');
+                    getStores();
+                    getBeverage();
+                    $scope.price = {};
+                }, function (error) {
+                    console.log(error);
+                });
+        }
+        $scope.closeModal(3);
+        $scope.AddPrice();
+    };
+
+    $scope.DeleteBeverage = function () {
+        Beverages.deleteBeverage(parseInt($stateParams.beverageId, 10))
+            .then(function (response) {
+                console.log('Beverage Deleted');
+                $state.go('tab.beverages');
+            }, function (error) {
+                console.log(error);
+            });
+    };
+
+})
+
 
 .controller('LocationCtrl', function ($scope, $cordovaGeolocation, $http) {
 
